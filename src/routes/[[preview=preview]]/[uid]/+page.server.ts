da7352e55ac6668f54f784821e3b22789313c8cf
Christian Kienle <105607540+chkie@users.noreply.github.com>
2024-08-20 22:07:47 +0200

    Update +page.server.ts

--- src/routes/[[preview=preview]]/[uid]/+page.server.ts
+++ src/routes/[[preview=preview]]/[uid]/+page.server.ts
@@ -4,27 +4,26 @@ import { createClient } from '$lib/prismicio';
 import { mappers } from '$lib/slices/mappers';
 
 export async function load({ params, fetch, cookies }) {
-	const client = createClient({ fetch, cookies });
+  const client = createClient({ fetch, cookies });
 
-	const page = await client.getByUID('page', params.uid);
+  const page = await client.getByUID('page', params.uid);
+  const slices = await mapSliceZone(page.data.slices, mappers, { client });
 
-	const slices = await mapSliceZone(page.data.slices, mappers, { client });
-
-	return {
-		slices,
-		title: asText(page.data.title),
-		meta_description: page.data.meta_description,
-		meta_title: page.data.meta_title,
-		meta_image: page.data.meta_image.url
-	};
+  return {
+    slices,
+    title: asText(page.data.title),
+    meta_description: page.data.meta_description,
+    meta_title: page.data.meta_title,
+    meta_image: page.data.meta_image.url
+  };
 }
 
 export async function entries() {
-	const client = createClient();
+  const client = createClient();
 
-	const pages = await client.getAllByType('page');
+  const pages = await client.getAllByType('page');
 
-	return pages.map((page) => {
-		return { uid: page.uid };
-	});
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
 }
