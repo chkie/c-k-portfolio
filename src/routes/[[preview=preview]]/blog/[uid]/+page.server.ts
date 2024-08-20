7cb255ada3ebe0e55fad64d5c51c7a44f5902fd6
chkie <105607540+chkie@users.noreply.github.com>
2024-08-20 17:33:30 +0200

    fix: try to fix the build error in vercel

--- src/routes/[[preview=preview]]/blog/[uid]/+page.server.ts
+++ src/routes/[[preview=preview]]/blog/[uid]/+page.server.ts
@@ -9,8 +9,8 @@ export async function load({ params, fetch, cookies }) {
     page,
     title: page.data.title,
     meta_description: page.data.meta_description,
-    meta_title: page.data.meta_title,
-    meta_image: page.data.meta_image || page.data.title
+    meta_title: page.data.meta_title || page.data.title,
+    meta_image: page.data.meta_image.url
   };
 }
 
