0e9bb212989cdf3f6080ad5f7e87a52ae4667b1f
chkie <105607540+chkie@users.noreply.github.com>
2024-08-26 21:13:43 +0200

    refactor: redesign scroll-up button to be mer consistent with the whole desing

--- src/lib/components/BackToTop.svelte
+++ src/lib/components/BackToTop.svelte
@@ -2,7 +2,6 @@
   import ScrollArrow from '~icons/material-symbols/arrow-upward-alt-rounded';
   export let showOnPx = 500;
   let hidden = true;
-  let rotation = 0;
   let lastScrollTop = 0;
   let isScrolling = false;
 
@@ -21,15 +20,6 @@
 
     const currentScrollTop = scrollContainer().scrollTop;
 
-    // Scrollrichtung prüfen
-    if (currentScrollTop > lastScrollTop) {
-      // Nach unten scrollen
-      rotation += 10; // Anpassung der Rotationsgeschwindigkeit
-    } else {
-      // Nach oben scrollen
-      rotation -= 10; // Anpassung der Rotationsgeschwindigkeit
-    }
-
     lastScrollTop = currentScrollTop;
     isScrolling = true;
 
@@ -38,33 +28,20 @@
     } else {
       hidden = true;
     }
-
-    // Setze isScrolling zurück, nachdem das Scrollen beendet ist
-    clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(() => {
-      isScrolling = false;
-    }, 100); // Timeout auf eine kleine Verzögerung setzen
   }
-
-  /**
-   * @type {string | number | NodeJS.Timeout | undefined}
-   */
-  let scrollTimeout;
 </script>
 
 <svelte:window on:scroll={handleOnScroll} />
 
 <button
-  class="w-14 h-14 rounded-full ransition-transform duration-300 md:w-20 md:h-20 md:right-20 md:bottom-20 bg-white/5 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-lg border-2 border-backdrop-blur-sm border-backdrop-filter border-white/5 hover:border-2 hover:border-cyan-500 fixed right-6 select-none bottom-6 hidden:opacity-0 visible:hidden z-50"
+  class="transition-transform duration-300 fixed md:bottom-20 w-14 h-14 md:w-20 md:h-20 md:right-20 right-6 bottom-6 rounded-full overflow-hidden group cursor-pointer bg-white/10 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 z-50 flex items-center justify-center shadow-lg"
   on:click={goTop}
-  class:hidden
-  style="transform: rotate({rotation}deg); transition: {isScrolling
-    ? 'transform 0.1s linear'
-    : 'none'};"
 >
-  <span class="flex justify-center">
-    <ScrollArrow
-      class=" transition-transform duration-300 cursor-pointer rounded-full w-10 h-10 md:w-16 md:h-16 hover:text-cyan-500"
-    />
-  </span>
+  <span
+    class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-cyan-500 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"
+  ></span>
+
+  <ScrollArrow
+    class="transition-transform duration-300 cursor-pointer rounded-full w-12 h-12 md:w-16 md:h-16 hover:text-slate-300 text-cyan-500 z-50"
+  />
 </button>
