293b2fd9ef4210174fb18531c99527a4f5a333e5
chkie <105607540+chkie@users.noreply.github.com>
2024-09-09 15:36:01 +0200

    add speed insights from vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,9 @@
   import ContactForm from '$lib/components/ContactForm.svelte';
   import { dev } from '$app/environment';
   import { inject } from '@vercel/analytics';
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+  injectSpeedInsights();
 
   inject({ mode: dev ? 'development' : 'production' });
 
